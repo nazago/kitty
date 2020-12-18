427e7650a4e1344e37050747493cbd16109a90f4
nazago <45107067+nazago@users.noreply.github.com>
2020-12-18 22:23:47 +0100

    Update threshold_identification.hpp

--- include/kitty/threshold_identification.hpp
+++ include/kitty/threshold_identification.hpp
@@ -10,120 +10,95 @@
 namespace kitty {
 
 
-    template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-    bool is_threshold(const TT& tt, std::vector<int64_t>* plf = nullptr)
+template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
+bool is_threshold(const TT& tt, std::vector<int64_t>* plf = nullptr)
     {
-      
         std::vector<int64_t> linear_form(tt.num_vars() + 1);
-	std::vector<bool> complemented_vars(tt.num_vars());
-	TT TF_TruthTable = tt;
-	std::vector<cube> On_set_vector = isop(TF_TruthTable);
-        std::vector<cube> Off_set_vector = isop(unary_not(TF_TruthTable));
+        std::vector<bool> inverted(tt.num_vars(), false);    
+        auto ttstar = tt;    
+        
+        for (uint8_t var = 0; var < tt.num_vars(); var++) {
+        
+         	  auto neg = cofactor0(tt, var);
+		  auto pos = cofactor1(tt, var);
 	
+            if ( implies(pos, neg ) ) {
+                flip_inplace(ttstar, var);
+                inverted.at(var) = true; 
+            }
+            else if  ( !implies(neg, pos) ) {
 
-	for (uint8_t i = 0u; i < tt.num_vars(); i++)
-	{
-	uint64_t flag_n, flag_p =0;
-	TT f0 = cofactor0(TF_TruthTable, i);
-	TT f1 = cofactor1(TF_TruthTable, i);
+                return false;
+            }
+        }
 
-
-	if (implies(f1,f0))
-	{
-		flag_n = 1;
-	}
-	else if (implies(f0,f1))
-	{
-		flag_p = 1;
-	} 
-	else
-	{
-	return false;
-	} 
-	
-
-	if (flag_n == 1)
-	{
-		flip_inplace( TF_TruthTable, i );
-		complemented_vars.at(i) = true;
-	}   
-	if (flag_p == 1)
-	{
-		complemented_vars.at(i) = false;
-	}  
-
-	} 
-
- 
-        
-        
-        
+    
         lprec* lp;
         int *colno = NULL;
         REAL *row = NULL;
         lp = make_lp(0, tt.num_vars() + 1);
-        for (int w = 1; w <= (int) tt.num_vars() + 1; w++) 
-        { 
- 
-            set_int(lp, w, TRUE);
+        
+        
+        for ( uint64_t i = 1; i < tt.num_vars() + 1; i++) { 
+         
+            set_int(lp, i, TRUE);
         }
+        
+        
         colno = (int *) malloc((tt.num_vars() + 1) * sizeof(*colno));
         row = (REAL *) malloc((tt.num_vars() + 1) * sizeof(*row));
-        if((colno == NULL) || (row == NULL))
-       	 return false;
+
+
+	std::vector<cube> onset_isop = isop(ttstar);
+        std::vector<cube> offset_isop = isop(unary_not(ttstar));
+        
+        
         set_add_rowmode(lp, TRUE); 
 
+        
+        
+        for (cube cube : onset_isop) { 
 
-        for (auto& ccube : On_set_vector) 
-        { 
+            for (uint8_t var = 0; var < tt.num_vars(); var++) { 
 
-            for (uint8_t k = 0; k < tt.num_vars(); k++) 
-            { 
-                auto ccube_without_literal = ccube;
-                ccube_without_literal.remove_literal(k);
+                auto cube_without_literal = cube;
+                cube_without_literal.remove_literal(var);
 
-               
-                 if (ccube.num_literals() != ccube_without_literal.num_literals())
-                 {
-                   
-                    colno[k] = k + 1;
-                    row[k] = 1;
-                 }
-                 else 
-                 {
-                    
-                    colno[k] = k + 1;
-                    row[k] = 0;
-                 }
+
+                if (cube.num_literals() != cube_without_literal.num_literals()) {
+              
+                    colno[var] = var + 1;
+                    row[var] = 1;
+                }
+                else {
+                    colno[var] = var + 1;
+                    row[var] = 0;
+                }
             }
             colno[tt.num_vars()] = tt.num_vars() + 1; 
-            row[tt.num_vars()] = -1;            
+            row[tt.num_vars()] = -1;            //
             add_constraintex(lp, tt.num_vars() + 1, row, colno, GE, 0);
         }
 
-       
-        
-        for (auto& ccube : Off_set_vector) 
-        { 
 
-            for (uint64_t m = 0; m < tt.num_vars(); m++) 
-            {
+ 
+        for (auto& cube : offset_isop) { 
 
-                auto ccube_without_literal = ccube;
-                ccube_without_literal.remove_literal(m);
+            for (uint8_t var = 0; var < tt.num_vars(); var++) { 
 
-                
-                if (ccube.num_literals() == ccube_without_literal.num_literals()) 
-                {
-                    
-                    colno[m] = m + 1;
-                    row[m] = 1;
+                auto cube_without_literal = cube;
+                cube_without_literal.remove_literal(var);
+
+
+                if (cube.num_literals() == cube_without_literal.num_literals()) {
+          
+                    colno[var] = var + 1;
+                    row[var] = 1;
                 }
-                else 
-                {
-                    
-                    colno[m] = m + 1;
-                    row[m] = 0;
+                else {
+     
+                    colno[var] = var + 1;
+                    row[var] = 0;
                 }
             }
             colno[tt.num_vars()] = tt.num_vars() + 1; 
@@ -131,66 +106,53 @@ namespace kitty {
             add_constraintex(lp, tt.num_vars() + 1, row, colno, LE, -1);
         }
 
-        
-       
+
+      
 
         set_add_rowmode(lp, FALSE); 
-
+     
+        for (uint8_t var = 0; var < tt.num_vars() + 1; var++) {
+            colno[var] = var + 1;
+            row[var] = 1;
+        }
         
-        for (uint64_t l = 0; l < tt.num_vars() + 1; l++) 
-        {
-            colno[l] = l + 1;
-            row[l] = 1;
+          for (uint8_t i = 0; i < tt.num_vars() + 1; i++) {
+            for (uint8_t j = 0; j < tt.num_vars() + 1; j++) {
+                colno[j] = j + 1;
+                row[j] = (i == j) ? 1 : 0;
+            }
+            add_constraintex(lp, tt.num_vars() + 1, row, colno, GE, 0);
         }
         set_obj_fnex(lp, tt.num_vars() + 1, row, colno);
 
-        
         set_verbose(lp, IMPORTANT);
         set_minim(lp);
         int result = solve(lp);
         get_variables(lp, row);
 
-        
-        for(uint64_t p = 0; p < tt.num_vars(); p++) 
-        {
-        if(complemented_vars[p])
-        {
-        linear_form[p] = -row[p];
-        row[tt.num_vars()] = row[tt.num_vars()] - row[p];
-        }
-        else
-        {
-        linear_form[p] = row[p];
-        }
+     
+        for(uint8_t var = 0; var < tt.num_vars(); var++) {
+            linear_form[var] = (inverted[var]) ? -row[var] : row[var];
+            row[tt.num_vars()] = (inverted[var]) ? row[tt.num_vars()] - row[var] : row[tt.num_vars()];
         }
         linear_form[tt.num_vars()] = (row[tt.num_vars()]);
 
         
         if(row != NULL)
-        {
             free(row);
-            }
         if(colno != NULL)
-        {
             free(colno);
-            }
-        if(lp != NULL)
-        { 
+        if(lp != NULL) 
             delete_lp(lp);
-            }
 
-        
         if(result == 0) {
             if(plf)
-            {
                 *plf = linear_form;
-                }
             return true;
         }
         else {
             return false;
         }
     }
-    }
-  
-    
+
+} /* namespace kitty */ 
